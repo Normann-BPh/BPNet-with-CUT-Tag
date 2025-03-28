edceab17ac187ddef3e6cf2ff11cea8f7f9c2613
Normann-BPh <jc.normann@outlook.com>
2025-03-29 00:08:35 +0100

    Create loss_valid_vs_train.py

---
+++ plotting/loss_valid_vs_train.py
@@ -0,0 +1,112 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
+TF_to_plot = input(' TF to train on. "HES1", "HEYL", "MYOD1" or "MYOG".\n Multi not supported.\n Press "enter" to choose "HES1": ')
+if TF_to_plot == '':
+    TF_to_plot = 'HES1'
+print('Using: ', TF_to_plot)
+
+colours = ["#79008a","#5bab07"]
+
+Validation_Loss = np.load(f'{TF_to_plot}_report_r/mnll_loss.npy', allow_pickle=True)
+
+Profile_Loss = np.load(f'{TF_to_plot}_report_r/profile_loss.npy', allow_pickle=True)
+
+ylim1_max = max(Validation_Loss)*(1.02)
+ylim1_min = np.round(max(Validation_Loss))*(0.98)
+
+ylim2_min = np.round(min(Validation_Loss*10000000))/10000000
+ylim2_max = ylim2_min * 1.08
+
+t = np.arange(len(Profile_Loss))
+
+if TF_to_plot in ['HES1','HEYL']:
+    fig, (ax1, ax2) = plt.subplots(2,1, sharex=True)
+    fig.subplots_adjust(hspace=0.05)
+
+    ax2.plot(t,Profile_Loss, color=colours[1])
+    
+    ax1.set_ylim(ylim1_min,ylim1_max)
+    ax1.get_yaxis().set_ticklabels([])
+    
+    ax1.spines.bottom.set_visible(False)
+    ax1.spines.top.set_visible(False)
+    ax2.spines.top.set_visible(False)
+    
+    ax2.xaxis.tick_bottom()
+    
+    ax1.tick_params(labeltop=False, bottom=False)
+    ax2.tick_params(axis='y', labelcolor=colours[1])
+    
+    ax2.set_ylabel('Training loss', color=colours[1], fontsize=18)
+    ax2.set_xlabel('Epoch', fontsize=18)
+
+    ax2.plot(t, Profile_Loss, color=colours[1])
+    
+    d = 0.5
+    kwargs = dict(marker=[(-1,-d),(1,d)],markersize=12,
+                    linestyle='none', color='k', mew=1, clip_on=False)
+    ax1.plot([0,1], [0,0], transform=ax1.transAxes, **kwargs)
+    ax2.plot([0,1], [1,1], transform=ax2.transAxes, **kwargs)
+
+    ax3 = ax1.twinx()
+    ax4 = ax2.twinx()
+    
+    ax3.set_ylim(ylim1_min,ylim1_max)
+    ax4.set_ylim(ylim2_min,ylim2_max)
+    
+    ax4.set_ylabel('Validation loss', color=colours[0], fontsize=18)
+    
+    ax3.tick_params(axis='y', labelcolor=colours[0])
+    ax4.tick_params(axis='y', labelcolor=colours[0])
+    
+    ax3.spines.bottom.set_visible(False)
+    ax4.spines.top.set_visible(False)
+
+    ax3.plot(t,Validation_Loss, color=colours[0])
+    ax4.plot(t,Validation_Loss, color=colours[0])
+    
+    ax1.spines.left.set_linewidth(1.5)
+    ax2.spines.left.set_linewidth(1.5)
+    
+    ax3.spines.right.set_linewidth(1.5)
+    ax4.spines.right.set_linewidth(1.5)
+    
+    ax1.spines.top.set_linewidth(1.5)
+    
+    ax2.spines.bottom.set_linewidth(1.5)
+    
+    
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    
+else:
+    fig, ax1 = plt.subplots()
+    
+    ax1.plot(t, Profile_Loss, color=colours[1])
+    ax1.tick_params(axis='y', labelcolor=colours[1])
+    
+    ax1.set_ylabel('Training loss', color=colours[1], fontsize=18)
+        
+    ax2 = ax1.twinx()    
+    ax2.set_ylim(ylim2_min,ylim1_max)
+    ax2.xaxis.tick_bottom()   
+    ax2.tick_params(axis='y', labelcolor=colours[0])
+    ax2.set_ylabel('Validation loss', color=colours[0], fontsize=18)
+    ax2.set_xlabel('Epoch', fontsize=18)
+    ax2.plot(t,Validation_Loss, color=colours[0])
+    
+    ax1.spines.left.set_linewidth(1.5)
+    ax2.spines.left.set_linewidth(1.5)
+    
+    ax1.spines.right.set_linewidth(1.5)
+    
+    ax1.spines.top.set_linewidth(1.5)
+    ax2.spines.top.set_linewidth(1.5)
+    
+    ax1.spines.bottom.set_linewidth(1.5)
+    
+    fig.tight_layout()  # otherwise the right y-label is slightly clipped
+    
+plt.savefig(f'results/plots/{TF_to_plot}_mnll_profile.png',dpi=400.0)
